c633b395c235de8936bf5077d444ca24b741a41f
huyao <huyao@meituan.com>
2014-03-28 16:19:19 +0800

    pager

--- src/com/xianglanqi/losesleep/MainActivity.java
+++ src/com/xianglanqi/losesleep/MainActivity.java
@@ -1,15 +1,73 @@
 package com.xianglanqi.losesleep;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class MainActivity extends Activity {
+
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // 将要分页显示的View装入数组中
+        LayoutInflater mLi = LayoutInflater.from(this);
+        View view1 = mLi.inflate(R.layout.page_home, null);
+        View view2 = mLi.inflate(R.layout.page_suggest, null);
+        View view3 = mLi.inflate(R.layout.page_dj, null);
+
+        // 每个页面的Title数据
+        final ArrayList<View> views = new ArrayList<View>();
+        views.add(view1);
+        views.add(view2);
+        views.add(view3);
+
+        final ArrayList<String> titles = new ArrayList<String>();
+        titles.add("tab1");
+        titles.add("tab2");
+        titles.add("tab3");
+
+        // 填充ViewPager的数据适配器
+        PagerAdapter mPagerAdapter = new PagerAdapter() {
+
+            @Override
+            public boolean isViewFromObject(View arg0, Object arg1) {
+                return arg0 == arg1;
+            }
+
+            @Override
+            public int getCount() {
+                return views.size();
+            }
+
+            @Override
+            public void destroyItem(View container, int position, Object object) {
+                ((ViewPager) container).removeView(views.get(position));
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return titles.get(position);
+            }
+
+            @Override
+            public Object instantiateItem(View container, int position) {
+                ((ViewPager) container).addView(views.get(position));
+                return views.get(position);
+            }
+        };
+
+        mViewPager.setAdapter(mPagerAdapter);
     }
 
 }
